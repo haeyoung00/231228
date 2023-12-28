b1405ee6c8941f744031a68c56029c809b37bbc6
heayoung0 <154854559+heayoung0@users.noreply.github.com>
2023-12-28 12:06:13 +0900

    호버기능 연습1

---
+++ js/script01.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $(".main > li, .img_area").mouseenter(function(){
+        $(".sub, .img_area").stop().show();
+
+    })//mouseenter
+    $(".main > li, .img_area").mouseleave(function(){
+        $(".sub, .img_area").stop().hide();
+    })//mouseleave
+
+    $(".main > li").mouseenter(function(){
+        var n = $(this).index();
+        console.log("n :", n)
+
+        $(".img_area .icons li").hide();
+        $(".img_area .icons li").eq(n).show();
+    })//mouseenter
+
+})//jquery
